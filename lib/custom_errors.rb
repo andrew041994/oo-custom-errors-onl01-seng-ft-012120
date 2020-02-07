01797ab06a8e277e11ae82f29c94843d57d2badc
Alehandro Persaud <andrew041994@gmail.com>
2020-02-07 03:41:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,14 +8,15 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
     else
       person.partner = self
     end
-  end
-
-  class PartnerError < StandardError
-  end
+end
 
 end
 
